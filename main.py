b32fa1c2161e6dca09b01051b4bdd9269996cb1e
Develku <110651468+Develku@users.noreply.github.com>
2023-11-12 10:51:38 +1100

    error hadling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+import time
 import streamlit as st
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
@@ -38,43 +39,46 @@ def pdf_to_document(uploaded_file):
 
 # Check if a file has been uploaded.
 if uploaded_file is not None:
-    # Process the uploaded PDF and get its pages.
-    pages = pdf_to_document(uploaded_file)
+    try:
+        # Process the uploaded PDF and get its pages.
+        pages = pdf_to_document(uploaded_file)
 
-    # Configure a text splitter for smaller, manageable chunks from the PDF.
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300, chunk_overlap=20, length_function=len, is_separator_regex=False)
+        # Configure a text splitter for smaller, manageable chunks from the PDF.
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=300, chunk_overlap=20, length_function=len, is_separator_regex=False)
 
-    # Split the PDF into smaller text segments.
-    texts = text_splitter.split_documents(pages)
+        # Split the PDF into smaller text segments.
+        texts = text_splitter.split_documents(pages)
 
-    # Instantiate OpenAI embeddings for numerical text representation.
-    embedding_model = OpenAIEmbeddings()
+        # Instantiate OpenAI embeddings for numerical text representation.
+        embedding_model = OpenAIEmbeddings()
 
-    # Set up a Chroma vector store with the text segments and embedding model.
-    db = Chroma.from_documents(texts, embedding_model)
+        # Set up a Chroma vector store with the text segments and embedding model.
+        db = Chroma.from_documents(texts, embedding_model)
 
-    # Interface for asking questions to the PDF.
-    st.header("Ask the PDF!")
-    question = st.text_input("Type your question")
+        # Interface for asking questions to the PDF.
+        st.header("Ask the PDF!")
+        question = st.text_input("Type your question")
 
-    # Button to trigger the question-answering process.
-    if st.button("Ask"):
-        if question:
-            # add spinner for waiting
-            with st.spinner('Wait for it...'):
-                time.sleep(5)
-            st.success('Done!')
+        # Button to trigger the question-answering process.
+        if st.button("Ask"):
+            if question:
+                # add spinner for waiting
+                with st.spinner('Wait for it...'):
+                    time.sleep(5)
+                st.success('Done!')
 
-            # Initialize a ChatOpenAI model with specific configurations.
-            llm = ChatOpenAI(model_name="gpt-4", temperature=0)
+                # Initialize a ChatOpenAI model with specific configurations.
+                llm = ChatOpenAI(model_name="gpt-4", temperature=0)
 
-            # Create a question-answering chain using the ChatOpenAI model and Chroma retriever.
-            qa_chain = RetrievalQA.from_chain_type(
-                llm, retriever=db.as_retriever())
-            result = qa_chain({"query": question})
+                # Create a question-answering chain using the ChatOpenAI model and Chroma retriever.
+                qa_chain = RetrievalQA.from_chain_type(
+                    llm, retriever=db.as_retriever())
+                result = qa_chain({"query": question})
 
-            # Display the result.
-            st.write(result["result"])
-        else:
-            st.error("Please enter a question.")
+                # Display the result.
+                st.write(result["result"])
+            else:
+                st.error("Please enter a question.")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
