e2f103ea885ca5aa975572467477a6779fcf4ce0
Develku <110651468+Develku@users.noreply.github.com>
2023-11-12 00:32:21 +1100

    final version for streamlit

--- main.py
+++ main.py
@@ -1,53 +1,81 @@
-# Import modules for PDF processing, text splitting, embedding generation, environment management,
-# vector storage, chat modeling, and retrieval-based question answering.
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from dotenv import load_dotenv
-from langchain.vectorstores import Chroma
-from langchain.chat_models import ChatOpenAI
+# Import necessary modules for various tasks including PDF handling, text splitting, embeddings,
+# vector storage, chat modeling, and question answering in Streamlit.
 from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
+import os
+import tempfile
+import streamlit as st
+import sys
 
-# Load environment variables for configurations such as API keys.
-load_dotenv()
+# Fix for SQLite issue in certain environments.
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-# Initialize a loader for the PDF file "romeo_juliet.pdf".
-loader = PyPDFLoader("romeo_juliet.pdf")
+# Initialize the Streamlit web application with a title and separator lines.
+st.title("ChatPDF")
+st.write("---")
 
-# Load the PDF and split it into individual pages.
-pages = loader.load_and_split()
+# Create an uploader widget to allow users to upload PDF files.
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+st.write("---")
 
-# Configure a text splitter for creating smaller text chunks from the PDF pages.
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=300,          # Defines the size of each chunk.
-    # Determines the overlap between consecutive chunks.
-    chunk_overlap=20,
-    length_function=len,     # Function to measure text length.
-    # Indicates whether a regex pattern is used as a separator.
-    is_separator_regex=False
-)
+# Define a function to handle the uploaded PDF file.
 
-# Split the PDF into smaller text segments.
-texts = text_splitter.split_documents(pages)
 
-# Create an instance of OpenAI Embeddings for numerical text representation.
-embedding_model = OpenAIEmbeddings()
+def pdf_to_document(uploaded_file):
+    # Create a temporary directory to store the uploaded PDF file.
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_filepath = os.path.join(temp_dir, uploaded_file.name)
+        with open(temp_filepath, "wb") as f:
+            f.write(uploaded_file.getvalue())
+        # Load the PDF and split it into pages.
+        loader = PyPDFLoader(temp_filepath)
+        pages = loader.load_and_split()
+        return pages
 
-# Set up a Chroma vector store with the text segments and embedding model.
-# This store will hold vector representations of the text for retrieval.
-db = Chroma.from_documents(texts, embedding_model)
 
-# Define a specific question for information retrieval.
-question = "What is the name of the main character in Romeo and Juliet?"
+# Check if the user has uploaded a PDF file.
+if uploaded_file is not None:
+    # Process the uploaded PDF to get its pages.
+    pages = pdf_to_document(uploaded_file)
 
-# Initialize a ChatOpenAI model with GPT-4 and a specified temperature.
-llm = ChatOpenAI(model_name="gpt-4", temperature=0)
+    # Configure a text splitter to divide the PDF into smaller text chunks.
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300, chunk_overlap=20, length_function=len, is_separator_regex=False)
 
-# Create a question-answering chain using the ChatOpenAI model and Chroma retriever.
-qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
+    # Split the PDF into smaller segments for processing.
+    texts = text_splitter.split_documents(pages)
 
-# Retrieve an answer to the question using the QA chain.
-result = qa_chain({"query": question})
+    # Initialize the OpenAI Embeddings model for text representation.
+    embedding_model = OpenAIEmbeddings()
 
-# Print the retrieved answer.
-print(result)
+    # Create a Chroma vector store with the text segments and the embedding model.
+    db = Chroma.from_documents(texts, embedding_model)
+
+    # Add an input section in the Streamlit app for users to type their questions.
+    st.header("Ask the PDF!")
+    question = st.text_input("Type your question")
+
+    # Add a button in the app for submitting the question.
+    if st.button("Ask"):
+        # Ensure the question is not empty before processing.
+        if question:
+            # Show a spinner while the question is being processed.
+            with st.spinner('Processing...'):
+                # Initialize the ChatOpenAI model with specific configurations.
+                llm = ChatOpenAI(model_name="gpt-4", temperature=0)
+                # Create a QA chain with the model and Chroma retriever for answering the question.
+                qa_chain = RetrievalQA.from_chain_type(
+                    llm, retriever=db.as_retriever())
+                # Retrieve the answer to the user's question.
+                result = qa_chain({"query": question})
+                st.success('Done!')
+                # Display the answer.
+                st.write(result["result"])
+        else:
+            # Display an error message if no question is entered.
+            st.error("Please enter a question.")
